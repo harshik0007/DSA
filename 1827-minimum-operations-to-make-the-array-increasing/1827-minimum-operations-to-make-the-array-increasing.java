f360034167e8866c583ec8b2182899036de3cca0
Tanti Harshik <tantiharshik@gmail.com>
2026-08-08 23:50:56 +0530

    Time: 3 ms (84.73%), Space: 47 MB (42.6%) - LeetHub

--- 1827-minimum-operations-to-make-the-array-increasing/1827-minimum-operations-to-make-the-array-increasing.java
+++ 1827-minimum-operations-to-make-the-array-increasing/1827-minimum-operations-to-make-the-array-increasing.java
@@ -2,9 +2,11 @@ class Solution {
     public int minOperations(int[] nums) {
         int count = 0;
         for(int i = 0; i < nums.length-1; i++){
-            if(nums[i] >= nums[i+1]){
-                int inc = nums[i] - nums[i+1];
-                nums[i+1] = nums[i+1] + inc+1;
+            int ele1 = nums[i];
+            int ele2 = nums[i+1];
+            if(ele1 >= ele2){
+                int inc = ele1 - ele2;
+                nums[i+1] = ele2 + inc+1;
                 count = count + inc+1;
             }
         }
