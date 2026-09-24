60e53f449f25cca4f4b77113989f055eb3bb8077
Tanti Harshik <tantiharshik@gmail.com>
2026-09-24 09:46:23 +0530

    Time: 8 ms (43.38%), Space: 42 MB (70%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,16 +1,14 @@
 class Solution {
-    public int fib(int n) {
+    public static int fiboSum(int n){
         if(n == 0 || n == 1){
             return n;
         }
-        int n1 = 0;
-        int n2 = 1;
-        for(int i = 2; i <= n; i++){
-            int temp = n1+n2;
-            n1 = n2;
-            n2 = temp;
-        }
 
-        return n2;
+        int ele = (fiboSum(n - 1) + fiboSum(n - 2));
+        return ele;
+    }
+    public int fib(int n) {
+        int sum = fiboSum(n);
+        return sum;
     }
 }
